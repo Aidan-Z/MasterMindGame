617b6ff97ac11780c24a5b615577b42670e7ee6c
Aidan-Z <aidanzamfir@gmail.com>
2023-06-03 20:44:25 -0300

    small modification to main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ options = ['R', 'G', 'B', 'P', 'O', 'Y']
 print(f"Color Options:\n{options}\n")
 
 combo = random.sample(options, 4) #random 4 colors
+print(combo)
 
 level = 1 #player attempts left
 def check(guess): #function, takes in 'guess' (list of users 4 letter input)
@@ -17,25 +18,18 @@ def check(guess): #function, takes in 'guess' (list of users 4 letter input)
             board[x] = i #change '-' with correct letter
 
             count += 1  # for each correct letter add 1 to letters
-    print(f"{count} of the letters you guessed are in the code")
+    print(f"{count}/4 letters are in code")
     return board
 
 
 while level <=8: #while still have attempts left
-
     print(f"Level: {level}")
-
-    # print(combo)
-    user_input = input("Guess 4 individual letters: ").upper() #user input letters
-    guess = [i for i in user_input] #convert to list 'guess'
-
+    user_input = input("Guess 4 individual letters: ").upper()
+    guess = [i for i in user_input]
     game_board = check(guess)
 
 
-    # switch order of these
     if game_board == combo:
-        print(game_board)
-        print(combo)
         print('YOU WIN')
         level += 10
     elif game_board != combo and level > 0:
